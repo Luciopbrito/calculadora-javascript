59b4563cbfb2df7c384868233c33abf4823ed430
Lúcio Brito <luciopereirabritoo@gmail.com>
2022-02-03 12:41:54 -0300

    modificado arquivo calcular.js

--- js/calcular.js
+++ js/calcular.js
@@ -7,47 +7,43 @@ botao.addEventListener('click', calcular)
 
 function calcular() {
     if (n1.value == '') {
-        alert('Valor inválido')
-        document.getElementById('txtn1').focus()
+        alert('Valor inválido');
+        document.getElementById('txtn1').focus();
     } else if (n2.value == '') {
-        alert('Valor inválido')
-        document.getElementById('txtn2').focus()
+        alert('Valor inválido');
+        document.getElementById('txtn2').focus();
     } else {
         switch (op.value) {
             case '+':
-                var resultado =  Number(n1.value) + Number(n2.value)
-                res.innerHTML = `A soma de ${n1.value} e ${n2.value} é igual a <strong>${resultado}</strong>` 
-                res.style.display = 'flex'
-                break
+                var resultado =  Number(n1.value) + Number(n2.value);
+                res.innerHTML = `A soma de ${n1.value} e ${n2.value} é igual a <strong>${resultado}</strong>`;
+                res.style.display = 'block';
+                res.style.textAlign = 'center'
+                break;
             case '-':
-                var resultado =  Number(n1.value) - Number(n2.value)
-                res.innerHTML = `A subtração de ${n1.value} e ${n2.value} é igual a <strong>${resultado}</strong>` 
-                res.style.display = 'flex'
-                break
+                var resultado =  Number(n1.value) - Number(n2.value);
+                res.innerHTML = `A subtração de ${n1.value} e ${n2.value} é igual a <strong>${resultado}</strong>`;
+                res.style.display = 'block';
+                res.style.textAlign = 'center'
+                break;
             case '/':
-                var resultado =  Number(n1.value) / Number(n2.value)
-                res.innerHTML = `A divisão de ${n1.value} e ${n2.value} é igual a <strong>${resultado}</strong>` 
-                res.style.display = 'flex'
-                break
+                var resultado =  Number(n1.value) / Number(n2.value);
+                res.innerHTML = `A divisão de ${n1.value} e ${n2.value} é igual a <strong>${resultado}</strong>`;
+                res.style.display = 'block';
+                res.style.textAlign = 'center'
+                break;
             case '*':
-                var resultado =  Number(n1.value) * Number(n2.value)
-                res.innerHTML = `A multiplicação de ${n1.value} e ${n2.value} é igual a <strong>${resultado}</strong>` 
-                res.style.display = 'flex'
-                break
             case 'X':
-                var resultado =  Number(n1.value) * Number(n2.value)
-                res.innerHTML = `A multiplicação de ${n1.value} e ${n2.value} é igual a <strong>${resultado}</strong>` 
-                res.style.display = 'flex'
-                break
             case 'x':
-                var resultado =  Number(n1.value) * Number(n2.value)
-                res.innerHTML = `A multiplicação de ${n1.value} e ${n2.value} é igual a <strong>${resultado}</strong>` 
-                res.style.display = 'flex'
-                break
+                var resultado =  Number(n1.value) * Number(n2.value);
+                res.innerHTML = `A multiplicação de ${n1.value} e ${n2.value} é igual a <strong>${resultado}</strong>`;
+                res.style.display = 'block';
+                res.style.textAlign = 'center'
+                break;
             default:
-                alert('Operação inválida')
-                document.getElementById('txtop').focus()
-                break
+                alert('Operação inválida');
+                document.getElementById('txtop').focus();
+                break;
         }
     }
 }
